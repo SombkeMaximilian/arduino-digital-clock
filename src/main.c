38ffb08b9d668927350e04eb112d05b0b83fabe1
SombkeMaximilian <sombkemaximilian@gmail.com>
2023-12-23 04:04:48 +0100

    added a short hello world program that blinks 4 LEDs (like a 4 bit data bus)

---
+++ src/main.c
@@ -0,0 +1,52 @@
+// -------------------------------------------------- //
+// dependencies
+
+# include <avr/io.h>
+# include <avr/interrupt.h>
+# include <util/delay.h>
+
+
+// -------------------------------------------------- //
+// constants
+
+# ifndef F_CPU
+# define F_CPU 16000000UL
+# endif
+# define mskDDRD (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5)
+
+
+// -------------------------------------------------- //
+// main
+
+int main (void) {
+    
+    setup();
+    
+    main_loop();
+    
+    return 0;
+    
+}
+
+// -------------------------------------------------- //
+// main loop of the program
+
+void main_loop (void) {
+    
+    while (1) {
+        
+        PORTD ^= mskDDRD;
+        _delay_ms(1000);
+        
+    }
+    
+}
+
+// -------------------------------------------------- //
+// configures the arduino pins
+
+void setup (void) {
+    
+    DDRD = mskDDRD;
+    
+}
