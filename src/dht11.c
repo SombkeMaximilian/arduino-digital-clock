8e2b2fe6bc7daf048f510fa2a6b16b1517c3e403
SombkeMaximilian <sombkemaximilian@gmail.com>
2024-01-28 12:19:13 +0100

    added templates for the library for a DHT11 humidity and temperature sensor

---
+++ src/dht11.c
@@ -0,0 +1,60 @@
+// -------------------------------------------------- //
+// dependencies
+
+# include <stdint.h>
+
+# include <avr/io.h>
+# include <util/delay.h>
+
+# include "dht11.h"
+# include "macros.h"
+
+
+// ------------------------------------------------------------ //
+// configuration of DHT11
+
+void DHT11config(DHT11 * dht11, uint8_t io) {
+    
+    
+    
+    
+    
+}
+
+
+// ------------------------------------------------------------ //
+// user command for retrieving temperature and humidity data
+
+void DHT11readData(DHT11 * dht11, DHT11data * data) {
+    
+    
+    
+    
+    
+    
+}
+
+
+// -------------------------------------------------- //
+// reads one byte of data from the DHT11
+
+void DHT11read(DHT11 * dht11) {
+    
+    
+    
+    
+    
+}
+
+
+// -------------------------------------------------- //
+// sends the start signal through io pin (datasheet page 6)
+
+void DHT11beginTransfer(DHT11 * dht11) {
+    
+    
+    
+    
+    
+    
+}
---
+++ src/dht11.h
@@ -0,0 +1,50 @@
+# ifndef DHT11_H
+# define DHT11_H
+
+// ------------------------------------------------------------ //
+// structs for storing temperature and humidity data
+
+typedef struct DHT11Data {
+    
+    uint8_t temp_integral;
+    uint8_t temp_decimal;
+    uint8_t humi_integral;
+    uint8_t humi_decimal;
+    uint8_t checksum;
+    
+} DHT11Data;
+
+
+// ------------------------------------------------------------ //
+// struct for storing information about the pins and settings
+
+typedef struct DHT11 {
+    
+    // io pin
+    uint8_t _io_pin;
+    
+    // current direction of io pin
+    uint8_t _io_dir;
+    
+} DHT11;
+
+
+// ------------------------------------------------------------ //
+// configuration of DHT11
+
+void DHT11config(DHT11 * dht11, uint8_t io);
+
+
+// ------------------------------------------------------------ //
+// user command for retrieving temperature and humidity data
+
+void DHT11readData(DHT11 * dht11, DHT11data * data);
+
+
+// ------------------------------------------------------------ //
+// functions for communicating with DHT11
+
+void DHT11read8bit(DHT11 * dht11);
+void DHT11beginTransfer(DHT11 * dht11);
+
+# endif
